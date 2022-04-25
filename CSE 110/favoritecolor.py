61ba19d1153de43b83fa97391e94de5c33d292dd
Sammie Atkins <atk21007@byui.edu>
2022-04-25 11:08:01 -0600

    first commit

---
+++ CSE 110/favoritecolor.py
@@ -0,0 +1,2 @@
+myVariable = input("What is your favorite color? ")
+print(f"{myVariable} is such a fun color!! :)")
---
+++ CSE 110/hello.py
@@ -0,0 +1,4 @@
+#this is a hello program
+print("\nhello, and welcome to CSE 110\n")
+myVariable = input("What is your favorite show? ")
+print("You said", myVariable)
