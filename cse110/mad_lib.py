b75a3ed889f7b95b9ad2f67e59dc229ee1f349de
Sammie Atkins <atk21007@byui.edu>
2022-04-28 15:30:46 -0600

    mad lib activity edit

--- cse110/mad_lib.py
+++ cse110/mad_lib.py
@@ -9,5 +9,5 @@ verb3 = input("verb: ")
 adjective2 = input("adjective: ")
 
 # Print story with user's inputs. 
-# Added single quotes because there are double quotes in the story.
+# Used single quotes because there are double quotes in the story.
 print(f'The other day, I was really in trouble. It all started when I saw a very {adjective.lower()} {animal.lower()} {verb1.lower()} down the hallway. "{exclamation.capitalize()}!" I yelled. But all I could think to do was to {verb2} over and over. Miraculously, that caused it to stop, but not before it tried to {verb3} right in front of my {adjective2} family. ')
