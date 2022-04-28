820d9ab57bc434a193798eaa0168220884b77aac
Sammie Atkins <atk21007@byui.edu>
2022-04-28 14:03:22 -0600

    name activity and renamed files

---
+++ cse110/name_string_practice.py
@@ -0,0 +1,6 @@
+# Ask for name.
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+
+# Print names. 
+print(f"Your name is {last_name}, {first_name} {last_name}.")
