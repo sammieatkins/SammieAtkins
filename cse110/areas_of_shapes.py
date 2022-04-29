d89a229b6dec68078b918d04f99db2acbb0d1654
Sammie Atkins <atk21007@byui.edu>
2022-04-29 12:41:02 -0600

    areas of shapes and meal price calculator

---
+++ cse110/areas_of_shapes.py
@@ -0,0 +1,17 @@
+from cmath import pi
+import math
+
+# # Square area calculator.
+# square_length = float(input("What is the length of a side of the square? "))
+# square_area = square_length ** 2
+# print(f"The area of the square is: {square_area}")
+
+# # Rectangle area calculator. 
+# rectangle_length = float(input("What is the length of the rectangle? "))
+# rectangle_width = float(input("What is the width of the rectangle? "))
+# print(f"The area of the rectangle is: {rectangle_length * rectangle_width}")
+
+# Circle area calculator. 
+circle_radius = float(input("What is the radius of the circle? "))
+print(f"The area of the circle is {pi * circle_radius ** 2}")
+
--- cse110/meal_price_calculator.py
+++ cse110/meal_price_calculator.py
@@ -5,19 +5,36 @@ child_num = int(input("How many child meals? "))
 adult_num = int(input("How many adult meals? "))
 sales_tax_rate = float(input("What is the sales tax rate? "))
 
-# Calculations.
+# Subtotal.
 subtotal = child_price * child_num + adult_price * adult_num
-print(f"Subtotal: {subtotal}.")
+print(f"Subtotal: {subtotal:.2f}.")
 
+# Calculations. 
 sales_tax = sales_tax_rate / 100 * subtotal
-print(f"Sales tax: {sales_tax}")
+print(f"Sales tax: {sales_tax:.2f}")
 
 total_price = sales_tax + subtotal
-print(f"Your total is {total_price}. :)")
+print(f"Your total is {total_price:.2f}.")
+
+# Add creativity. (Tip)
+tip_percentage = float(input("Please enter tip percentage: "))
+if tip_percentage >= 20:
+    print("Thank you!! :) ")
+elif tip_percentage == 0:
+    print("How rude :( ")
+else:
+    print("Thank you :)")
+tip = tip_percentage / 100 * subtotal
+
+# Total with tip.
+total_tip = total_price + tip
+print(f"Your total is now {total_tip:.2f}")
 
 # Payment & Change.
 payment = float(input("What is the payment amount? "))
-change = payment - total_price
-print(f"Your change is {change}.")
-
-# Add creativity. 
+change = payment - total_tip
+print(f"Your change is {change:.2f}.")
+if tip_percentage >= 1:
+    print("Thanks for dining with us :)")
+elif tip_percentage ==0:
+    print("Goodbye.")
