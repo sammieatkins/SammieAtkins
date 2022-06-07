6fc773e90f27239e16370c30c1043f7bbfea9180
Sammie Atkins <atk21007@byui.edu>
2022-06-07 11:18:23 -0600

    class jun seventh

---
+++ cse111/practice/heart_rate.py
@@ -0,0 +1,16 @@
+"""
+When you physically exercise to strengthen your heart, you
+should maintain your heart rate within a range for at least 20
+minutes. To find that range, subtract your age from 220. This
+difference is your maximum heart rate per minute. Your heart
+simply will not beat faster than this maximum (220 - age).
+When exercising to strengthen your heart, you should keep your
+heart rate between 65% and 85% of your heart's maximum rate.
+"""
+
+age = int(input("Please enter your age: "))
+max_heart_rate = 220 - age
+slowest_rate = 65 * max_heart_rate / 100
+fastest_rate = 85 * max_heart_rate / 100
+
+print(f"When you exercise to strengthen your heart, you should keep your heart rate between {slowest_rate:.0f} and {fastest_rate:.0f} beats per minute. ")
---
+++ cse111/team_activities/pendulum.py
@@ -0,0 +1,29 @@
+"""
+The time in seconds that a pendulum takes to swing back and
+forth once is given by this formula:
+             ____
+            / h
+    t = 2π / ----
+          √  9.81
+
+t is the time in seconds,
+π is the constant PI, which is the ratio of the circumference
+    of a circle divided by its diameter (use math.pi),
+h is the length of the pendulum in meters.
+
+Write a program that prompts a user to enter the length of a
+pendulum in meters and then computes and prints the time in
+seconds that it takes for that pendulum to swing back and forth.
+"""
+import math
+
+# input
+height = float(input("Length of pendulum (meters): "))
+
+# calculations
+square_root = math.sqrt(height / 9.81)
+time = 2 * math.pi * square_root
+# 2 * math.pi * math.sqrt(height / 9.81)
+
+# display
+print(f"Time (seconds): {time:.2f}")
