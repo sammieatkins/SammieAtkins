b82343b3874acf55c7429f73739e5bb58a033aad
Sammie Atkins <atk21007@byui.edu>
2022-06-08 16:31:09 -0600

    tire volume fix

---
+++ cse111/prove/tire_volume.py
@@ -0,0 +1,35 @@
+"""
+Enter the width of the tire in mm (ex 205): 185
+Enter the aspect ratio of the tire (ex 60): 50
+Enter the diameter of the wheel in inches (ex 15): 14
+
+The approximate volume is 24.09 liters
+
+> python tire_volume.py
+Enter the width of the tire in mm (ex 205): 205
+Enter the aspect ratio of the tire (ex 60): 60
+Enter the diameter of the wheel in inches (ex 15): 15
+
+The approximate volume is 39.92 liters
+
+v is the volume in liters,
+π is the constant PI, which is the ratio of the circumference of a circle divided by its diameter (use math.pi),
+w is the width of the tire in millimeters,
+a is the aspect ratio of the tire, and
+d is the diameter of the wheel in inches.
+
+"""
+# imports
+import math
+
+# inputs
+width = float(input("Enter the width of the tire in mm (ex 205): "))
+aspect_ratio = float(input("Enter the aspect ratio of the tire (ex 60): "))
+diameter = float(input("Enter the diameter of the wheel in inches (ex 15): "))
+
+# calculations
+    # WHaat is going on here - DONT USE COMMAS TO SEPARATE BIG NUMBERS
+volume = (math.pi * width ** 2 * aspect_ratio * (width * aspect_ratio + 2540 * diameter)) / 10000000000
+
+# display
+print(f"The approximate volume is {volume:.2f} liters. ")
