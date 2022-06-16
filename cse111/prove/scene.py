6c8a4aa9d72a21ae8943565815fd9cf515bd206c
Sammie Atkins <atk21007@byui.edu>
2022-06-15 19:47:34 -0600

    test

--- cse111/prove/scene.py
+++ cse111/prove/scene.py
@@ -3,9 +3,11 @@
 from draw2d import \
     start_drawing, draw_line, draw_oval, draw_arc, \
     draw_rectangle, draw_polygon, draw_text, finish_drawing
+import tkinter
 
 
 def main():
+    print(tkinter.TkVersion)
     # Width and height of the scene in pixels
     scene_width = 800
     scene_height = 500
@@ -13,14 +15,16 @@ def main():
     # Call the start_drawing function in the draw2d.py
     # library which will open a window and create a canvas.
     canvas = start_drawing("Scene", scene_width, scene_height)
-    draw_rectangle(canvas, 0, 0, scene_width, scene_height, fill="lightSkyBlue")
+
     # Call your drawing functions such
     # as draw_sky and draw_ground here.
-    draw_pine_tree(canvas, 550, 150, 250)
-    draw_pine_tree(canvas, 200, 100, 200)
+        # draw_pine_tree(canvas, 550, 150, 250)
+        # draw_pine_tree(canvas, 200, 100, 200)
+    draw_sky(canvas, scene_width, scene_height)
+    draw_ground(canvas, scene_width, 150)
 
-    for x in range(100, 700, 100):
-        draw_pine_tree(canvas, x, 250, 80)
+    # for x in range(100, 700, 100):
+    #     draw_pine_tree(canvas, x, 250, 80)
 
     draw_grid(canvas, scene_width, scene_height, 50)
 
@@ -33,26 +37,50 @@ def main():
 # Define your functions such as
 # draw_sky and draw_ground here.
 
-def draw_pine_tree(canvas, center_x, bottom, height):
-    # Draw the tree trunk.
-        # pass in lower left and upper right coordinates
-    trunk_width = height / 10
-    trunk_height = height / 8
-    left_trunk = center_x - trunk_width / 2
-    bottom_trunk = bottom
-    right_trunk = center_x + trunk_width / 2
-    trunk_top = bottom + trunk_height
-    draw_rectangle(canvas, left_trunk, bottom_trunk, right_trunk, trunk_top, fill="tan4")
+def draw_sky(canvas, scene_width, scene_height):
+    draw_rectangle(canvas, 0, 0, scene_width, scene_height, fill="skyBlue1")
+    #cloud 1
+    draw_cloud(canvas, 150, 350, 100, 100)
 
-    # Draw the skirt of the tree
-    skirt_width = height / 2
-    skirt_left = center_x - skirt_width / 2
-    skirt_bottom = trunk_top
-    peak_x = center_x
-    peak_y = bottom + height
-    skirt_right = center_x + skirt_width / 2
+# def draw_clouds 
+    # make this overlap many circles to make an actual cloud :)
+
+
+def draw_ground(canvas, scene_width, height):
+    draw_rectangle(canvas, 0, height, scene_width, 0, fill="tan4")
+
+def draw_cloud(canvas, center_x, bottom, width, height):
+    top_x = center_x - width / 2
+    top_y = bottom + height
+    bottom_x = center_x + width / 2
+    # bottom_y is bottom
+
+    draw_oval(canvas, top_x, top_y, bottom_x, bottom, fill="white")
+
+    pass
+
+
+
+# def draw_pine_tree(canvas, center_x, bottom, height):
+#     # Draw the tree trunk.
+#         # pass in lower left and upper right coordinates
+#     trunk_width = height / 10
+#     trunk_height = height / 8
+#     left_trunk = center_x - trunk_width / 2
+#     bottom_trunk = bottom
+#     right_trunk = center_x + trunk_width / 2
+#     trunk_top = bottom + trunk_height
+#     draw_rectangle(canvas, left_trunk, bottom_trunk, right_trunk, trunk_top, fill="tan4")
+
+#     # Draw the skirt of the tree
+#     skirt_width = height / 2
+#     skirt_left = center_x - skirt_width / 2
+#     skirt_bottom = trunk_top
+#     peak_x = center_x
+#     peak_y = bottom + height
+#     skirt_right = center_x + skirt_width / 2
     
-    draw_polygon(canvas, skirt_left, skirt_bottom, peak_x, peak_y, skirt_right, skirt_bottom, fill="forestGreen")
+#     draw_polygon(canvas, skirt_left, skirt_bottom, peak_x, peak_y, skirt_right, skirt_bottom, fill="forestGreen")
 
 def draw_grid(canvas, width, height, interval):
     # Draw vertical line. 
